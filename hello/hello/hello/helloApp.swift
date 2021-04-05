278a17f5317d9b1de586039016bfb84489a9895f
aganzai <aganzai@ganchaongdeiMac.lan>
2021-04-05 22:12:24 +0800

    add hello world project

---
+++ hello/hello/hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by aganzai on 2021/4/5.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
